1f2e025bfc220b158b031dc6162bc9d4996ac8a3
Emir <emirbesirx@gmail.com>
2025-07-09 20:45:01 +0300

    feat: add level ending UI

---
+++ GameT11/Assets/_Game/Scripts/UI/LevelEndingPanel.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelEndingPanel : MonoBehaviour
+{
+
+
+    [Header("UI Elements")]
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private GameObject starsParentObject;
+
+    [Header("Player Data")]
+    [SerializeField] private FloatVariable playerScore;
+
+    [Header("Panel Sprites")]
+    [SerializeField] private Sprite winPanelSprite;
+    [SerializeField] private Sprite losePanelSprite;
+
+    [Header("Star Sprites")]
+    [SerializeField] private Sprite starSprite;
+    [SerializeField] private Sprite emptyStarSprite;
+
+
+    private const string winText = "TESLIMAT BASARILI";
+    private const string loseText = "TESLIMAT BASARISIZ";
+
+
+    public void InitializeEndingPanel(FloatVariable levelPassScore)
+    {
+        UpdateBasePanel(levelPassScore.Value);
+        UpdateScoreText();
+        UpdateStarDisplay(levelPassScore.Value);
+    }
+
+    private void UpdateBasePanel(float levelPassScore)
+    {
+        if (playerScore.Value >= levelPassScore)
+        {
+            titleText.text = winText;
+            backgroundImage.sprite = winPanelSprite;
+        }
+        else
+        {
+            titleText.text = loseText;
+            backgroundImage.sprite = losePanelSprite;
+        }
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = ((int) playerScore.Value).ToString();
+    }
+
+    private void UpdateStarDisplay(float levelPassScore)
+    {
+        int starCount = GetStarCount(levelPassScore);
+        for (int i = 0; i < starsParentObject.transform.childCount; i++)
+        {
+            Image starImage = starsParentObject.transform.GetChild(i).GetComponent<Image>();
+            if (i < starCount)
+            {
+                starImage.sprite = starSprite;
+            }
+            else
+            {
+                starImage.sprite = emptyStarSprite;
+            }
+        }
+    }
+
+    private int GetStarCount(float levelPassScore)
+    {
+        int starCount = 0;
+
+        if (playerScore.Value >= levelPassScore * 1.25f)
+        {
+            starCount = 3;
+        }
+        else if (playerScore.Value >= levelPassScore * 1.15f)
+        {
+            starCount = 2;
+        }
+        else if (playerScore.Value >= levelPassScore)
+        {
+            starCount = 1;
+        }
+
+        return starCount;
+    }
+}
