0a0ee2381b33c687788fbbaa2fdafb2697bf94fa
Emir <emirbesirx@gmail.com>
2025-07-11 04:56:28 +0300

    feat: add pause screen

---
+++ GameT11/Assets/_Game/Scripts/Core/TimeScaleManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TimeScaleManager : MonoBehaviour
+{
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
+    }
+    
+    public void SetTimeScale(float newTimeScale)
+    {
+        Time.timeScale = newTimeScale;
+    }
+}
